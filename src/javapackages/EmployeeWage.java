ef4a59c28121e9ff807a67af9bb54d587b0f8040
shubham shirke <shirkeshubham84@gmail.com>
2023-03-05 20:22:12 +0530

    Added:Calculating Daily Wage if the employee is Present

--- src/javapackages/EmployeeWage.java
+++ src/javapackages/EmployeeWage.java
@@ -1,16 +1,21 @@
 package javapackages;
+
 import java.util.Random;
+
 public class EmployeeWage {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Random random = new Random();
         int a = random.nextInt(10)+1;
+        int wage_per_hour=20;
+        int full_day_hour=8;
         if(a%2==0){
-
             System.out.println("Employee is Present ");
+            int daily_wage = wage_per_hour*full_day_hour;
+            System.out.println("Daily wage of the employee is: "+daily_wage+" Rupees");
         }
         else{
             System.out.println("Employee is absent");
+            System.out.println("Daily wage of the employee is: 0 Rupees");
         }
-
     }
 }
