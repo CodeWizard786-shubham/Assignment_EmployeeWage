79a833e629936fe76714bbc0ebf984abc8c223a0
shubham shirke <shirkeshubham84@gmail.com>
2023-03-05 20:45:22 +0530

    Added:Part time employee Wage with option to choose Employee type

--- src/javapackages/EmployeeWage.java
+++ src/javapackages/EmployeeWage.java
@@ -1,21 +1,40 @@
 package javapackages;
-
-import java.util.Random;
+import java.util.Scanner;
+import java.lang.Math;
 
 public class EmployeeWage {
-    public static void main(String[] args){
-        Random random = new Random();
-        int a = random.nextInt(10)+1;
+    public static void main(String args[]){
+        double emp_rand=Math.floor(Math.random()*10)%2;
+        Scanner sc =new Scanner(System.in);
         int wage_per_hour=20;
         int full_day_hour=8;
-        if(a%2==0){
-            System.out.println("Employee is Present ");
-            int daily_wage = wage_per_hour*full_day_hour;
-            System.out.println("Daily wage of the employee is: "+daily_wage+" Rupees");
+        int part_time_hour=8;
+        System.out.println("Enter 1 if you are a full time Employee\nEnter 2 if you are Part time Employee ");
+        int choice = sc.nextInt();
+        if(choice==1) {
+            if (emp_rand == 1) {
+                System.out.println("Employee is Present ");
+                int daily_wage = wage_per_hour * full_day_hour;
+                System.out.println("Daily wage of the employee is: " + daily_wage + " Rupees");
+            } else {
+                System.out.println("Employee is absent");
+                System.out.println("Daily wage of the employee is: 0 Rupees");
+            }
+        }
+        else if(choice==2){
+            if(emp_rand==0){
+                System.out.println("Part time Employee is Present ");
+                int daily_wage_partTime = part_time_hour * wage_per_hour;
+                System.out.println("Daily wage of the employee is: " + daily_wage_partTime+ " Rupees");
+
+            }
+            else{
+                System.out.println("Part time employee is absent");
+            }
         }
         else{
-            System.out.println("Employee is absent");
-            System.out.println("Daily wage of the employee is: 0 Rupees");
+            System.out.println("Wrong Choice");
         }
     }
 }
+
