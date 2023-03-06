e12e89248ced18e7a176b9a62ecf6f87d273ecf6
shubham shirke <shirkeshubham84@gmail.com>
2023-03-06 17:25:41 +0530

    Added:Calculating Employee Monthly Wage

--- src/javapackages/EmployeeWage.java
+++ src/javapackages/EmployeeWage.java
@@ -1,49 +1,21 @@
 package javapackages;
-import java.util.Scanner;
 import java.lang.Math;
 
 public class EmployeeWage {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         int wage_per_hour = 20;
         int full_day_hour = 8;
-        int part_time_hour = 8;
-        System.out.println("Enter 1 if you are a full time Employee\nEnter 2 if you are Part time Employee ");
-        int choice = sc.nextInt();
-        switch (choice) {
-            case 1:
-                double emp_random1 = Math.floor(Math.random() * 10) % 2;
-                int emp_rand1 = (int) emp_random1;
-                switch (emp_rand1) {
-                    case 1:
-                        System.out.println("Employee is Present ");
-                        int daily_wage = wage_per_hour * full_day_hour;
-                        System.out.println("Daily wage of the employee is: " + daily_wage + " Rupees");
-                        break;
-                    case 0:
-                        System.out.println("Employee is absent");
-                        System.out.println("Daily wage of the employee is: 0 Rupees");
-                        break;
-                }
-                break;
-            case 2:
-                double emp_random2 = Math.floor(Math.random() * 10) % 2;
-                int emp_rand2 = (int) emp_random2;
-                switch (emp_rand2) {
-                    case 1:
-                        System.out.println("Part time Employee is Present ");
-                        int daily_wage_partTime = part_time_hour * wage_per_hour;
-                        System.out.println("Daily wage of Part time employee is: " + daily_wage_partTime + " Rupees");
-                        break;
-                    case 0:
-                        System.out.println("Part time employee is absent");
-                        break;
-                }
-                break;
-            default: {
-                System.out.println("Wrong Choice");
+        int wage=0;
+        for(int i=1;i<=20;i++) {
+            double emp_rand1 =(int) Math.floor(Math.random() * 10) % 2;
+            if (emp_rand1 == 1) {
+                System.out.println("Employee is Present");
+                int Dailywage = wage_per_hour * full_day_hour;
+                wage=wage+Dailywage;
+            } else {
+                System.out.println("Employee is Absent");
             }
         }
-    }
-}
-
+        System.out.println("Total Monthly Wage of the employee is= "+wage);
+            }
+        }
