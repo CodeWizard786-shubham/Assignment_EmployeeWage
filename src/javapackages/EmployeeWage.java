2c695f61ac5e655c031177010c627f73ca70516f
shubham shirke <shirkeshubham84@gmail.com>
2023-03-16 22:09:09 +0530

    Added: class cmethod with function parameter

--- src/javapackages/EmployeeWage.java
+++ src/javapackages/EmployeeWage.java
@@ -1,15 +1,25 @@
 package javapackages;
-import java.util.Scanner;
 import java.lang.Math;
-class Employee {
-    int wage_per_hour = 20;
-    int full_day_hour = 8;
-    int part_time_hour = 4;
-    int monthly_wage = 0;
-    int month_days = 0;
-    int total_working_hour = 0;
+
+public class EmployeeWage {
+    private final String company;
+    private final int numOfWorkingDays;
+    private final int wage_per_hour;
+    private int total_working_hour;
+    private final int full_day_hour;
+    private final int part_time_hour;
+
+    public EmployeeWage(String company,int wage_per_hour,int full_day_hour,int part_time_hour,int numOfWorkingDays,int total_working_hour ){
+        this.company=company;
+        this.wage_per_hour = wage_per_hour;
+        this.numOfWorkingDays=numOfWorkingDays;
+        this.total_working_hour=total_working_hour;
+        this.full_day_hour=full_day_hour;
+        this.part_time_hour=part_time_hour;
+    }
+
     public int employeeRandom(){
-        double emp_random1 = Math.floor(Math.random() * 10) % 2;
+        double emp_random1 = Math.floor(Math.random() * 10) % 3;
         int emp_rand1 = (int) emp_random1;
         return emp_rand1;
     }
@@ -26,57 +36,64 @@ class Employee {
         return daily_wage;
 
     }
+    public int getNumOfWorkingDays(){
+        return numOfWorkingDays;
+    }
+    public int getTotal_working_hour(){
+        return total_working_hour;
+    }
+    public String getCompany(){
+        return company;
+    }
 
-}
-public class EmployeeWage {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Employee e1=new Employee();
-        System.out.println("Enter 1 if you are a full time Employee\nEnter 2 if you are Part time Employee ");
-        int choice = sc.nextInt();
+        int month_days=0,total_working_hour=0,numOfWorkingDays=0,monthly_wage=0;
+        EmployeeWage dMart=new EmployeeWage("DMart",20,8,10,20,4);
+        int choice=dMart.employeeRandom();
         switch (choice) {
             case 1:
-                while (e1.month_days != 20 || e1.total_working_hour != 100) {
+                while (numOfWorkingDays !=dMart.getNumOfWorkingDays() || total_working_hour != dMart.getTotal_working_hour()) {
 
-                    int emp_random=e1.employeeRandom();
-                    e1.month_days += 1;
+                    int emp_random=dMart.employeeRandom();
+                    month_days += 1;
                     switch (emp_random) {
                         case 1:
                             System.out.println("Employee is Present ");
-                            e1.monthly_wage=e1.monthly_wage+e1.dailyWageFullTime();
+                            monthly_wage=monthly_wage+dMart.dailyWageFullTime();
                             break;
-                        case 0:
+                        case 2:
                             System.out.println("Employee is absent");
                             break;
                     }
-                    if (e1.month_days == 20 || e1.total_working_hour == 100) {
+                    if ((month_days == dMart.getNumOfWorkingDays()) || (total_working_hour == dMart.getTotal_working_hour())) {
                         break;
                     }
                 }
-                System.out.println("Monthly wage of Full time employee is: " + e1.monthly_wage);
+                System.out.println("Monthly wage of Full time employee is: " +monthly_wage);
                 break;
             case 2:
-                while (e1.month_days != 20 || e1.total_working_hour != 100) {
-                    int emp_random=e1.employeeRandom();
-                    e1.month_days += 1;
+                while (month_days !=dMart.getNumOfWorkingDays() || total_working_hour != dMart.getTotal_working_hour()) {
+                    int emp_random=dMart.employeeRandom();
+                      month_days += 1;
                     switch (emp_random) {
                         case 1:
                             System.out.println("Part time Employee is Present ");
-                            e1.monthly_wage=e1.monthly_wage+e1.dailyWagePartTime();
+                            monthly_wage=monthly_wage+dMart.dailyWagePartTime();
                             break;
-                        case 0:
+                        case 2:
                             System.out.println("Part time employee is absent");
                             break;
                     }
-                    if (e1.month_days == 20 || e1.total_working_hour == 100) {
+                    if (month_days == dMart.getNumOfWorkingDays()|| total_working_hour == dMart.getTotal_working_hour()) {
                         break;
                     }
                 }
-                System.out.println("Monthly wage of Part time employee is: " + e1.monthly_wage);
+                System.out.println("Monthly wage of Part time employee is: " +monthly_wage);
                 break;
             default: {
                 System.out.println("Wrong Choice");
             }
         }
+        System.out.println("Name of the company is:"+dMart.getCompany());
     }
 }
